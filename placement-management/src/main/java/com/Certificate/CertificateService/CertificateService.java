678928b0386b95f03714a0fa284c1622d0ed34c2
Sanjay-v-arch <sanjayofficial1302@gmail.com>
2025-10-17 00:03:03 +0530

    updated

--- placement-management/src/main/java/com/Certificate/CertificateService/CertificateService.java
+++ placement-management/src/main/java/com/Certificate/CertificateService/CertificateService.java
@@ -1,4 +1,4 @@
-package certificateService;
+package com.Certificate.CertificateService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
